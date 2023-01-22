f61a0dc04260775d3532de9a83e86e08f8456d86
abdulmaleekadams <harbdulmaleek@gmail.com>
2023-01-22 15:27:19 -0800

    Add footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,5 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
+import styles from "./Footer.module.scss";
+import clsx from "clsx";import { Red_Rose } from "@next/font/google";
+
+const redRose = Red_Rose({weight: "400", subsets: ["latin"] });
 
 const Footer = () => {
   const footerLinks = [
@@ -19,34 +23,45 @@ const Footer = () => {
 
   const social = ["facebook", "instagram", "twitter"];
   return (
-    <footer>
-      <div>
-        <Image
-          src="/whitemetabnblogo.svg"
-          alt="Metabnb logo"
-          width={120}
-          height={30}
-        />
-        <div></div>
-        {social.map((title) => (
-          <div key={title}>
-            <a href={`#${title}`} aria-label={`Metabnb ${title} Link`}>
-              <Image src={`/${title}.svg`} height={25} width={25} alt={title} />
-            </a>
-          </div>
-        ))}
-      </div>
-      <div>
-        {footerLinks.map((link) => (
-          <div key={link}>
-            <p>{link.title}</p>
-            {link.links.map((link, idx) => (
-              <Link href="#" key={idx}>
-                {link}
-              </Link>
+    <footer className={clsx(styles.footer, redRose.className)}>
+      <div className={clsx(styles.main)}>
+        <div className={clsx(styles.brand)}>
+          <Image
+            src="/whitemetabnblogo.svg"
+            alt="Metabnb logo"
+            width={200}
+            height={30}
+            className={clsx(styles.logo)}
+          />
+          <div className={clsx(styles.social)}>
+            {social.map((title) => (
+              <div key={title}>
+                <a href={`#${title}`} aria-label={`Metabnb ${title} Link`}>
+                  <Image
+                    src={`/${title}.svg`}
+                    height={18}
+                    width={18}
+                    alt={title}
+                  />
+                </a>
+              </div>
             ))}
           </div>
-        ))}
+        </div>
+        <div className={clsx(styles.links)}>
+          {footerLinks.map((link) => (
+            <div key={link}>
+              <p>{link.title}</p>
+              <div className={clsx(styles.link)}>
+                {link.links.map((link, idx) => (
+                  <Link href="#" key={idx}>
+                    {link}
+                  </Link>
+                ))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
     </footer>
   );
